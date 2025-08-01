f47dfd87b655306c27b7d051d5db23850bba5383
daniilraptanov <daniilraptanov@gmail.com>
2025-08-01 22:55:06 +0300

    refactor: simplify components

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
-import {
-  defineConfig
-} from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  // root: "",
-  build: {
-    outDir: "dist",
-    // emptyOutDir: true,
-    // lib: {
-    //   entry: path.resolve(__dirname, "src/main.js"),
-    //   name: "MyLib",
-    //   formats: ["iife"],
-    //   fileName: (format) => `my-lib.js`
-    // },
-  },
-  server: {},
-  plugins: [],
+    // root: "",
+    build: {
+        outDir: "dist",
+        // emptyOutDir: true,
+        // lib: {
+        //   entry: path.resolve(__dirname, "src/main.js"),
+        //   name: "MyLib",
+        //   formats: ["iife"],
+        //   fileName: (format) => `my-lib.js`
+        // },
+    },
+    server: {},
+    plugins: [],
 });
